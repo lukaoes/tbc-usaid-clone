7cb5b6a428dc4095bad95dc4c2e1f52e9a167424
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-01-21 00:52:53 +0400

    add about section

---
+++ JS/data.js
@@ -0,0 +1,4 @@
+// about section information
+const aboutData = "„ TBC x USAID - ტექნოლოგიური განათლებისთვის “ პროგრამა  საინფორმაციო ტექნოლოგიებით დაინტერესებულ ადამიანებს გთავაზობთ სრულად დაფინანსებულ ონლაინ საგანმანათლებლო პრაქტიკულ კურსებს  სხვადასხვა ICT მიმართულებით. წარმატებულ კურსდამთავრებულებს ეძლევათ შესაძლებლობა დასაქმდნენ თიბისისა და მის პარტნიორ კომპანიებში.";
+const aboutParagraph = document.getElementById('aboutParagraph');
+aboutParagraph.textContent = aboutData;
