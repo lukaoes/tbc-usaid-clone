47c2b6ff253b5fdcaae58a2925db9b065e22ab09
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-01-21 15:37:28 +0400

    make FAQ questions generator

--- JS/data.js
+++ JS/data.js
@@ -3,6 +3,8 @@ const aboutData = "„ TBC x USAID - ტექნოლოგიური გა
 const aboutParagraph = document.getElementById('aboutParagraph');
 aboutParagraph.textContent = aboutData;
 
+
+// course grid data and fucntion to generate them
 const courseDetailedData = [
     {
         id: 'ios-course',
@@ -70,7 +72,7 @@ const courseDetailedData = [
 ];
 
 const generateCourses = (data) => {
-    const courseGrid = document.getElementById('courseGrid')
+    const courseGrid = document.getElementById('courseGrid');
     let courseCard = "";
     data.forEach((element) => {
         courseCard = courseCard + `
@@ -95,9 +97,89 @@ const generateCourses = (data) => {
         </div>
     </div>
         `
-        console.log(element)
     });
     courseGrid.innerHTML = courseCard
 }
 
-generateCourses(courseDetailedData)
+generateCourses(courseDetailedData);
+
+// FAQ data, anwers and questions.
+const faqData = [
+    {
+        id: 1,
+        question: 'როგორ ხდება კურსებზე რეგისტრაცია და შერჩევა?',
+        answer: `
+        <p>
+            კურსზე რეგისტრაციისთვის უნდა გაიარო რამდენიმე ეტაპი:
+        </p>
+        <ul>
+            <li>
+                <span>I ეტაპი</span> - პირველ ეტაპზე საჭიროა, შეავსო სასურველი კურსისთვის განკუთვნილი სარეგისტრაციო ფორმა, რომელიც განთავსებულია 
+                კურსის შიდა გვერდზე. კურსზე რეგისტრაციის დასრულების შემდეგ დაიწყება შემოსული განცხადებების გადარჩევა. 
+            </li>
+            <li>
+                <span>I ეტაპი</span> - შერჩევის მეორე ეტაპი კურსების მიხედვით განსხვავებულია, ზოგიერთი კურსისთვის მოიცავს პრე-ტესტს, ზოგიერთ კურსზე 
+                კი უნარების ტესტს, სადაც მინიმალური ზღვარის გადალახვის შემთხვევაში გადახვალ შერჩევის შემდეგ ეტაპზე.
+            </li>
+            <li>
+                <span>I ეტაპი</span> - მესამე ეტაპი კურსების მიხედვით განსხვავდება: Advance კურსებზე, სადაც მოითხოვება გარკვეული ტექნიკური ცოდნა, 
+                მონაწილეებმა უნდა დაწერონ ტექნიკური ტესტი ცოდნის დონის შესამოწმებლად, ხოლო კურსებზე, სადაც რაიმე ტიპის წინასწარი ცოდნა 
+                მოთხოვნილი არ არის უნდა შეასრულოთ ტექნიკური დავალება, რაც თქვენი კვლევისა და თვითსწავლის უნარს ამოწმებს.
+            </li>
+            <li>
+                <span>I ეტაპი</span> - შერჩევის ბოლო მეოთხე ეტაპი მოიცავს გასაუბრებას შესარჩევ კომისიასთან. გასაუბრების წარმატებით გავლის შემთხვევაში 
+                ჩაირიცხებით კურსზე და გაფორმდება შესაბამისი ხელშეკრულება.
+            </li>
+            <li>
+        </ul>
+        <p>
+            * სანამ კურსზე დარეგისტრირდები მნიშვნელოვანია, ყურადღებით წაიკითხო კურსის აღწერა, ნახო რას ისწავლი კურსის განმავლობაში 
+            და გაიგო გააჩნია თუ არა კურსს დასწრების წინაპირობა. 
+        </p>
+        `
+    },
+    {
+        id: 2,
+        question: 'შემიძლია თუ არა ერთზე მეტ კურსზე რეგისტრაცია?',
+        answer: `<p>
+                    TBC X USAID ტექნოლოგიური განათლებისთვის პროგრამაში თითოეულ კანდიდატს აქვს მხოლოდ ერთი კურსის გავლის შესაძლებლობა.
+                    გარდა Information Security და Python კურსებისა, სადაც Basic დონის გავლის შემდეგ შესაძლებელია სწავლის გაგრძელება Advance დონეზე.
+                </p>`
+    },
+    {
+        id: 3,
+        question: 'რა ღირს სწავლა პროგრამის ფარგლებში?',
+        answer: `<p>
+                    პროგრამის ფარგლებში კურსებზე სწავლა სრულიად დაფინანსებულია თიბისი ბანკისა და USAID-ის მიერ.
+                </p>`
+    },
+];
+
+// generate FAQ section content
+const generateFaq = (data) => {
+    const questionAnswer = document.getElementById('question-answer');
+    let faqQs = "";
+    data.forEach((element) => {
+        faqQs = faqQs + `
+        <div class="question-container" onclick="toggleAnswer(${element.id})">
+            <div class="question-block flex">
+                <div class="question">
+                    <h3>
+                        ${element.question}
+                    </h3>
+                </div>
+                <div class="faq-arrow arrow-${element.id}">
+                    <img src="./assets/images/faq-arrow.svg" alt="faq-arrow" />
+                </div>
+            </div>
+            <div class="answer  answer-${element.id}">
+                ${element.answer}
+            </div>
+        </div>
+        `
+    });
+    questionAnswer.innerHTML = faqQs
+};
+
+generateFaq(faqData);
+console.log(faqData)
