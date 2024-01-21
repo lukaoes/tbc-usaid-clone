0392c82f96ea326733f8e638bbaba7649a8e6c24
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-01-21 14:21:49 +0400

    update FAQ accordion

--- JS/functions.js
+++ JS/functions.js
@@ -9,7 +9,27 @@ const toggleMenu = () => {
     mobileNavigation.style.opacity = mobileNavigation.style.right === '0px' ? '1' : '0.3';
 
     // overlay on main content
-
     const menuOpenedOverlay = document.getElementById('menu-opened-overlay');
     menuOpenedOverlay.style.display = mobileNavigation.style.right === '0px' ? 'block' : 'none';
 };
+
+// FAQ questions accordion 
+const toggleAnswer = (qnum) => {
+    const answer = document.querySelector(`.answer-${qnum}`);
+    const arrow = document.querySelector(`.arrow-${qnum}`);
+    const isAnswerOpen = answer.classList.contains('answer-open');
+
+    const closeAll = (selector, className) => {
+        const allElements = document.querySelectorAll(selector);
+        allElements.forEach((element) => element.classList.remove(className));
+    };
+
+    // closeo pened elemnts
+    closeAll('.answer', 'answer-open');
+    closeAll('.faq-arrow', 'open-arrow');
+
+    if (!isAnswerOpen) {
+        answer.classList.toggle('answer-open');
+        arrow.classList.toggle('open-arrow');
+    }
+}
