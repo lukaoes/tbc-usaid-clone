2b1339fa39d8a926fbb5fa9a61f6dd1e33b1c760
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-01-22 13:04:06 +0400

    add slides dinamically with js

--- JS/data.js
+++ JS/data.js
@@ -183,3 +183,50 @@ const generateFaq = (data) => {
 
 generateFaq(faqData);
 console.log(faqData)
+
+// slider 
+const slidesData = [
+    [
+        "./assets/images/partner-imgs/usaid-partner.png",
+        "./assets/images/partner-imgs/space-partner.png",
+        "./assets/images/partner-imgs/tnet-partner.png"
+    ],
+    [
+        "./assets/images/partner-imgs/tegeta-partner.png",
+        "./assets/images/partner-imgs/spectre-partner.png",
+        "./assets/images/partner-imgs/tbc-partner.png"
+    ],
+    [
+        "./assets/images/partner-imgs/ufc-partner.png"
+    ]
+];
+
+function createSlide(images) {
+    const slideDiv = document.createElement("div");
+    slideDiv.className = "slide";
+
+    images.forEach((imageSrc) => {
+        const imageDiv = document.createElement("div");
+        const img = document.createElement("img");
+        img.src = imageSrc;
+
+        const imageName = imageSrc.split("/").pop().replace("-partner.png", "");
+        img.alt = imageName
+
+        imageDiv.appendChild(img);
+        slideDiv.appendChild(imageDiv);
+    });
+
+    return slideDiv;
+}
+
+function generateSlides() {
+    const slidesContainer = document.querySelector('.slides');
+
+    slidesData.forEach((images, index) => {
+        const slide = createSlide(images, index);
+        slidesContainer.appendChild(slide);
+    });
+}
+
+generateSlides();
